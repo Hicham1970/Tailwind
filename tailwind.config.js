d6f6d9e91f695140570572b25655817cdb4cd50f
Hicham1970 <hicham.garroum@sgs.com>
2024-07-18 11:22:53 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html, css}", "./public/outputs.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
